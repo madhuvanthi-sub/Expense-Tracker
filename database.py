2cf9bff9263c4c045efbb47e930fb7c1ab6e9d6b
madhu-subramanian <podamadhu2004@gmail.com>
2026-09-09 16:23:25 +0530

    Expense functions in database.py

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 import sqlite3
+DATABASE_NAME = "expenses.db"
 def create_database():
-    connection = sqlite3.connect("expenses.db")
+    connection = sqlite3.connect(DATABASE_NAME)
     cursor = connection.cursor()
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS expenses (
@@ -13,6 +14,67 @@ def create_database():
     """)
     connection.commit()
     connection.close()
+def add_expense(date, amount, category, description):
+    connection = sqlite3.connect(DATABASE_NAME)
+    cursor = connection.cursor()
+    cursor.execute("""
+        INSERT INTO expenses
+        (date, amount, category, description)
+        VALUES (?, ?, ?, ?)
+    """, (date, amount, category, description))
+    connection.commit()
+    connection.close()
+def get_expenses():
+    connection = sqlite3.connect(DATABASE_NAME)
+    cursor = connection.cursor()
+    cursor.execute("""
+        SELECT id, date, amount, category, description
+        FROM expenses
+        ORDER BY date DESC
+    """)
+    expenses = cursor.fetchall()
+    connection.close()
+    return expenses
+def delete_expense(expense_id):
+    connection = sqlite3.connect(DATABASE_NAME)
+    cursor = connection.cursor()
+    cursor.execute(
+        "DELETE FROM expenses WHERE id = ?",
+        (expense_id,)
+    )
+    connection.commit()
+    connection.close()
+def update_expense(
+    expense_id,
+    date,
+    amount,
+    category,
+    description
+):
+    connection = sqlite3.connect(DATABASE_NAME)
+    cursor = connection.cursor()
+    cursor.execute("""
+        UPDATE expenses
+        SET date = ?,
+            amount = ?,
+            category = ?,
+            description = ?
+        WHERE id = ?
+    """, (
+        date,
+        amount,
+        category,
+        description,
+        expense_id
+    ))
+    connection.commit()
+    connection.close()
 if __name__ == "__main__":
     create_database()
-    print("Database created successfully!")
+    add_expense(
+        "09-09-2026",
+        250,
+        "Food",
+        "Lunch"
+    )
+    print(get_expenses())
